ff575d2cb31ba859e44a6dbdcf6984643f95acf4
Daniel Osborne <danielosborne@daniels-mbp.home>
2021-11-21 10:27:10 +0000

    Add a result struct and switch to using.

--- parkrun_aggregator.go
+++ parkrun_aggregator.go
@@ -4,14 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type result struct {
+	id          int
+	name        string
+	ageGroup    string
+	club        string
+	clubId      int
+	gender      string
+	position    int
+	runs        int
+	ageGrade    float32
+	achievement string
+	time        string
+	previousPB  string
+}
+
 // TODO: Create result objects and event objects (and runner?)
 
-func getEvent(location string, eventNum int) []map[string]string {
+func getEvent(location string, eventNum int) []result {
 	// TODO: Return object.
 	resp, err := http.Get(fmt.Sprintf("https://www.parkrun.org.uk/%v/results/%v/", location, eventNum))
 	if err != nil {
@@ -28,27 +44,35 @@ func getEvent(location string, eventNum int) []map[string]string {
 		// TODO: Change error handling?
 		log.Fatal(err)
 	}
-	results := make([]map[string]string, 0, 500) // TODO: REVIEW capacity of 500.
+	results := make([]result, 0, 500) // TODO: REVIEW capacity of 500.
 	doc.Find(".Results-table-row").Each(func(i int, s *goquery.Selection) {
-		result := make(map[string]string)
-		result["id"] = s.Find(".Results-table-td.Results-table-td--name").Find(".compact").Find("a").AttrOr("href", "")
-		result["id"] = result["id"][strings.LastIndex(result["id"], "/")+1:]
-		result["name"] = s.AttrOr("data-name", "")
-		result["agegroup"] = s.AttrOr("data-agegroup", "")
-		result["club"] = s.AttrOr("data-club", "")
-		result["clubId"] = s.Find(".Results-table-club.Results-tablet").Find(".detailed").Find("a").AttrOr("href", "")
-		result["clubId"] = result["clubId"][strings.LastIndex(result["clubId"], "=")+1:]
-		result["gender"] = s.AttrOr("data-gender", "")
-		result["position"] = s.AttrOr("data-position", "")
-		result["runs"] = s.AttrOr("data-runs", "")
-		result["agegrade"] = s.AttrOr("data-agegrade", "")
-		result["achievement"] = s.AttrOr("data-achievement", "")
-		result["time"] = s.Find(".Results-table-td.Results-table-td--time").Find(".compact").Text()
-		result["previousPB"], _ = s.Find(".Results-table-td.Results-table-td--time").Find(".detailed").Html() // TODO: Handle error.
-		result["previousPB"] = strings.ReplaceAll(result["previousPB"], "<span class=\"Results-table--normal\">", "")
-		result["previousPB"] = strings.ReplaceAll(result["previousPB"], "</span>", "")
-		result["previousPB"] = strings.ReplaceAll(result["previousPB"], "</span>&nbsp;", " ")
-		results = append(results, result)
+		idStr := s.Find(".Results-table-td.Results-table-td--name").Find(".compact").Find("a").AttrOr("href", "")
+		id, _ := strconv.Atoi(idStr[strings.LastIndex(idStr, "/")+1:]) // TODO: Handle error.
+		clubIdStr := s.Find(".Results-table-club.Results-tablet").Find(".detailed").Find("a").AttrOr("href", "")
+		clubId, _ := strconv.Atoi(clubIdStr[strings.LastIndex(clubIdStr, "=")+1:])                   // TODO: Handle error.
+		previousPB, _ := s.Find(".Results-table-td.Results-table-td--time").Find(".detailed").Html() // TODO: Handle error.
+		previousPB = strings.ReplaceAll(previousPB, "<span class=\"Results-table--normal\">", "")
+		previousPB = strings.ReplaceAll(previousPB, "<span class=\"Results-table--red\">", "")
+		previousPB = strings.ReplaceAll(previousPB, "<span class=\"Results-table--green\">", "")
+		previousPB = strings.ReplaceAll(previousPB, "</span>", "")
+		previousPB = strings.ReplaceAll(previousPB, "</span>&nbsp;", " ")
+		position, _ := strconv.Atoi(s.AttrOr("data-position", ""))           // TODO: Handle error.
+		runs, _ := strconv.Atoi(s.AttrOr("data-runs", ""))                   // TODO: Handle error.
+		ageGrade, _ := strconv.ParseFloat(s.AttrOr("data-agegrade", ""), 32) // Still made as float64 but convertable to 32. // TODO: Handle error.
+		results = append(results, result{
+			id:          id,
+			clubId:      clubId,
+			previousPB:  previousPB,
+			name:        s.AttrOr("data-name", ""),
+			ageGroup:    s.AttrOr("data-agegroup", ""),
+			club:        s.AttrOr("data-club", ""),
+			gender:      s.AttrOr("data-gender", ""),
+			position:    position,
+			runs:        runs,
+			ageGrade:    float32(ageGrade),
+			achievement: s.AttrOr("data-achievement", ""),
+			time:        s.Find(".Results-table-td.Results-table-td--time").Find(".compact").Text(),
+		})
 	})
 	return results
 }
