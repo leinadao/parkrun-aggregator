65c88d5decb2b7b9949a1ae0aae24c372f8eead1
Daniel Osborne <danaosb@gmail.com>
2021-11-22 22:47:10 +0000

    Remove unwanted clubsCapacity const.

--- club.go
+++ club.go
@@ -1,8 +1,5 @@
 package main
 
-// clubsCapacity constant is the initial slice capacity to be used for event clubs.
-const clubsCapacity = 500 // TODO: REVIEW capacity of 500.
-
 // club is a running club registered against a Parkrun attendee.
 type club struct {
 	id   int
